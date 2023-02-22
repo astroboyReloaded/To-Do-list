4f442dd5ad400b48d26e96e6370d4d39adbc1b65
Alex Muñoz <astroboy.reloaded@gmail.com>
2023-02-22 11:21:03 -0600

    Linters fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import './style.css';
 
 const tasks = [
-    {
-        description: 'Create an index.js file',
-        completed: false,
-        index: 1
-    },
-    {
-        description: 'Create a style.css file',
-        completed: false,
-        index: 2
-    },
-    {
-        description: 'Bundle into dist/ folder',
-        completed: false,
-        index: 3
-    }
+  {
+    description: 'Create an index.js file',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Create a style.css file',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Bundle into dist/ folder',
+    completed: false,
+    index: 3,
+  },
 ];
 
 const list = document.getElementById('taskList');
 
-const iterateTasks = () => tasks.map(task => `
+const iterateTasks = () => tasks.map(task => (`
 <li class="list-item">
-    <label class="task-label"><input class="check" type="checkbox">${task.description}</label>
+  <label class="task-label"><input class="check" type="checkbox">${task.description}</label>
 </li>
-`).join('');
+`)).join('');
 
 list.innerHTML = iterateTasks();
